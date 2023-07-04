0b7b720ba237ea48583e52492d2fd5a20924c5e1
Amine Nokra <amine.nokra@gmail.com>
2023-07-04 11:51:17 +0200

    Transaltion in italian

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World from Develop branch");
+        System.out.println("Ciao a tutti dal branch Develop");
     }
 }
